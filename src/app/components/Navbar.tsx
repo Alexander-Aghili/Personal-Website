e24095be4fd53636e48c02102bd613237b0ae902
Alex <awaghili@ucsc.edu>
2023-09-30 21:46:13 -0700

    First commit with navbar and such

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,29 @@
+// components/Navbar.js
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <Link href="/">Home</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/about">About</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li className={styles.navItem}>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
