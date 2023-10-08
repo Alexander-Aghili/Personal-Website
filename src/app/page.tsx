707c3c359c03e3164d70430b606039bc979d865d
Alex <awaghili@ucsc.edu>
2023-10-07 20:56:35 -0700

    Started working on home page, adjusted portfolio page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,26 +11,21 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
-      <h1>
-        Alexander Aghili
-      </h1>
-      <div className={styles.profileImage}>
-      <div className={styles.circularProfileImage}>
-        <Image
-          src={profileImage}
-          alt="Alexander Aghili's Profile"
-          width={100} // Adjust the width as needed
-          height={100} // Adjust the height as needed
-        />
+      <div className={styles.smallBio}>
+        <h1>
+          Alexander Aghili
+        </h1>
+        <div className={styles.profileImage}>
+        <div className={styles.circularProfileImage}>
+          <Image
+            src={profileImage}
+            alt="Alexander Aghili's Profile"
+            width={100} // Adjust the width as needed
+            height={100} // Adjust the height as needed
+          />
+        </div>
+        </div>
       </div>
-      </div>
-      <div className={styles.description}>
-        <p>
-          Welcome to My Personal Website!
-        </p>
-      </div>
-
-      
     </main>
   )
 }
