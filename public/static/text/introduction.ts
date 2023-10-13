dae01a131c970384f6eddd1cf16dc3a5d42adf71
Alex <awaghili@ucsc.edu>
2023-10-12 20:42:30 -0700

    Small style changes, working on fixing posts but still stuck

--- public/static/text/introduction.ts
+++ public/static/text/introduction.ts
@@ -1,3 +1,3 @@
-const text='Hello, my name is Alex. I am a Sophomore at the University of California, Santa Cruz majoring in Computer Science and Mathematics. ';
+const text='Hello, my name is Alex. I am a Sophomore at the University of California, Santa Cruz majoring in Computer Science and Mathematics. I hope to utilize software to improve the world and increase automation. ';
 
 export default text;
