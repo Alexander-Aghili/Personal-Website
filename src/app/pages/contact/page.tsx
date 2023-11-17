87caaa607eb709cc258b274af3eeb229c2b4b542
Alex <awaghili@ucsc.edu>
2023-11-16 22:57:06 -0800

    I hate React, I hate nextjs, I really hate CORS. This shit fucking sucks balls I wish it was not this shit

--- src/app/pages/contact/page.tsx
+++ src/app/pages/contact/page.tsx
@@ -1,71 +1,61 @@
-"use client"
-import React, { useState } from "react";
-import style from './page.module.css';
-import Navbar from "@/app/components/Navbar/Navbar";
 
-import { Resend } from 'resend';
+import * as React from "react";
 
-const ContactForm = () => {
-  const [submitted, setSubmitted] = useState(false);
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+interface EmailTemplateProps {
+  firstName: string;
+}
 
-    const inputs = e.target.elements;
-    const data: any = {};
+export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
+  firstName,
+}) => (
+  <div>
+    <h1>Welcome, {firstName}!</h1>
+  </div>
+);
 
-    for (let i = 0; i < inputs.length; i++) {
-      if (inputs[i].name) {
-        data[inputs[i].name] = inputs[i].value;
-      }
-    }
+export default EmailTemplate;
 
-    const resend = new Resend("re_DqBHJnAU_Lbuhc6h1EG387egLnVaSgKoj");
-    // resend.emails.send({
-    //   from: data["email"],
-    //   to: 'alexander.w.aghili@gmail.com',
-    //   subject: data["name"],
-    //   html: data["message"]
-    // });
-  };
+// const ContactForm = () => {
+//   const [submitted, setSubmitted] = useState(false);
 
-  if (submitted) {
-    return (
-      <div className={style.main}>
-        <Navbar></Navbar>
-        <div className={style.thanks}>
-          <h2>Thank you!</h2>
-          <div>We'll be in touch soon.</div>
-        </div>
-      </div>
-    );
-  }
+//   if (submitted) {
+//     return (
+//       <div className={style.main}>
+//         <Navbar></Navbar>
+//         <div className={style.thanks}>
+//           <h2>Thank you!</h2>
+//           <div>We'll be in touch soon.</div>
+//         </div>
+//       </div>
+//     );
+//   }
 
-  return (
+//   return (
     
-    <div
-    className={style.main}
-    >
-      <Navbar></Navbar>
-      <form
-      onSubmit={handleSubmit}
-      method="POST"
-      className={style.form}
-      >
-      <div>
-          <input className={style.text} type="text" placeholder="Your name" name="name" required />
-      </div>
-      <div>
-          <input className={style.text} type="email" placeholder="Email" name="email" required />
-      </div>
-      <div>
-          <textarea className={style.message} placeholder="Your message" name="message" required />
-      </div>
-      <div>
-          <button className={style.submit} type="submit"> Send a message </button>
-      </div>
-      </form>
-    </div>
-  );
-};
+//     <div
+//     className={style.main}
+//     >
+//       <Navbar></Navbar>
+//       <form
+//       onSubmit={handleSubmit}
+//       method="POST"
+//       className={style.form}
+//       >
+//       <div>
+//           <input className={style.text} type="text" placeholder="Your name" name="name" required />
+//       </div>
+//       <div>
+//           <input className={style.text} type="email" placeholder="Email" name="email" required />
+//       </div>
+//       <div>
+//           <textarea className={style.message} placeholder="Your message" name="message" required />
+//       </div>
+//       <div>
+//           <button className={style.submit} type="submit"> Send a message </button>
+//       </div>
+//       </form>
+//     </div>
+//   );
+// };
 
-export default ContactForm;
+// export default ContactForm;
